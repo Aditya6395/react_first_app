b5413841acf1fd5b3ef6a6ff1deec3395c6b0b07
aditya <aaityachauhan6395gmail.com>
2024-11-19 12:54:57 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom';
+
+// Create a simple App component
+function App() {
+  return (
+    <div>
+      <h1>Aditya Chauhan,You have Craete a first React App</h1>
+    </div>
+  );
+}
+
+
+ReactDOM.render(
+<>
+<App/>
+</>,
+  document.getElementById('root') // Assuming you have a root div in your HTML
+);
+
+
